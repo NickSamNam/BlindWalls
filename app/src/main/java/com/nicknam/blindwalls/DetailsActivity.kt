936c134bbb672593d82f90f5708ca996460f4606
Sam Nicknam <s.nicknam@icloud.com>
2017-11-09 21:05:31 +0100

    Made detailed view functional

--- app/src/main/java/com/nicknam/blindwalls/DetailsActivity.kt
+++ app/src/main/java/com/nicknam/blindwalls/DetailsActivity.kt
@@ -2,11 +2,27 @@ package com.nicknam.blindwalls
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.squareup.picasso.Picasso
+import jp.wasabeef.picasso.transformations.BlurTransformation
+import kotlinx.android.synthetic.main.activity_details.*
+import java.util.*
 
 class DetailsActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
+        val wall = intent.getSerializableExtra("wall") as Wall
+        val l = if (resources.configuration.locale.language == Locale("nl").language) "nl" else "en"
+
+        activity_details_tv_nr.text = wall.nrOnMap.toString()
+        activity_details_tv_year.text = wall.year.toString()
+        activity_details_tv_title.text = wall.title[l]
+        activity_details_tv_address.text = wall.address
+        activity_details_rb_rating.rating = wall.rating ?: 0f
+        activity_details_tv_material.text = getString(R.string.Material, wall.material[l])
+        activity_details_tv_categorie.text = getString(R.string.Categorie, wall.category[l])
+        activity_details_tv_description.text = wall.description[l]
+        Picasso.with(this).load(wall.images.firstOrNull()).into(activity_details_iv_wall)
+        Picasso.with(this).load(wall.images.firstOrNull()).transform(BlurTransformation(this)).into(activity_details_iv_wall_blurred)
     }
 }
