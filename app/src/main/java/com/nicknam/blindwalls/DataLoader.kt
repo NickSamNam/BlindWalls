d553715e18f3b34ac1d1b57141097eb5ee0b623f
Sam Nicknam <s.nicknam@icloud.com>
2017-11-09 18:12:57 +0100

    Created loader for json in resource Added walls.json file to raw resources

---
+++ app/src/main/java/com/nicknam/blindwalls/DataLoader.kt
@@ -0,0 +1,14 @@
+package com.nicknam.blindwalls
+
+import android.content.Context
+import org.json.JSONArray
+import java.io.InputStreamReader
+
+/**
+ * Created by snick on 9-11-2017.
+ */
+class DataLoader {
+    companion object {
+        fun retreiveWalls(context: Context): JSONArray = JSONArray(InputStreamReader(context.resources.openRawResource(R.raw.walls)).buffered().use { it.readText() })
+    }
+}
